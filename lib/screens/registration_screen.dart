7d34a6f5a0b529eb4e856f19999600e028338b99
Kunal <kunal.rohitas29@gmail.com>
2021-08-11 13:42:10 +0530

    Made the registration page flexible

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -25,9 +25,14 @@ class _RegistrationScreenState extends State<RegistrationScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              height: 200.0,
-              child: Image.asset('images/logo.png'),
+            Flexible(
+              child: Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
+              ),
             ),
             SizedBox(
               height: 48.0,
